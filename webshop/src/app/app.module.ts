89e3ee8f90eaf457ac297fea60cd6583de52d936
GregoryTheRat <bankigergely0519@gmail.com>
2023-05-05 19:56:42 +0200

    firestore added

--- webshop/src/app/app.module.ts
+++ webshop/src/app/app.module.ts
@@ -7,6 +7,12 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideStorage,getStorage } from '@angular/fire/storage';
+import { AngularFireModule } from '@angular/fire/compat';
 
 
 @NgModule({
@@ -19,7 +25,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserAnimationsModule,
     MatIconModule,
     MatToolbarModule,
-    MatButtonModule
+    MatButtonModule,
+
+    AngularFireModule,
+    //provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage())
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ webshop/src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'webfejl-project-2023-e0e55',
+    appId: '1:451834122537:web:44b0544bc70d2c24336738',
+    storageBucket: 'webfejl-project-2023-e0e55.appspot.com',
+    apiKey: 'AIzaSyB6ZJhLYzorP2TaFkAoV7ap8HIlqia4ulI',
+    authDomain: 'webfejl-project-2023-e0e55.firebaseapp.com',
+    messagingSenderId: '451834122537',
+    measurementId: 'G-SW7BSEWM66',
+  },}
