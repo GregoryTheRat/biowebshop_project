a6c8d296ae38429a60dcba87f8f75f80e94ff3e6
GregoryTheRat <bankigergely0519@gmail.com>
2023-05-06 17:11:06 +0200

    user CRUD

--- webshop/src/app/pages/register/register.component.ts
+++ webshop/src/app/pages/register/register.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
+import { UserService } from '../../shared/services/user.service';
+import { User } from '../../shared/models/User';
 import { AuthService } from '../../shared/services/auth.service';
 
 @Component({
@@ -16,11 +18,22 @@ export class RegisterComponent {
     password: new FormControl('')
   });
 
-  constructor(private router: Router, private authService: AuthService) { }
+  constructor(private router: Router, private authService: AuthService, 
+    private userService: UserService) { }
 
   onSubmit(){
     this.authService.signup(this.signUpForm.get('email')?.value, this.signUpForm.get('password')?.value).then( cred => {
         console.log(cred);
+        const user: User = {
+          id: cred.user?.uid as string,
+          email: this.signUpForm.get('email')?.value,
+          username: this.signUpForm.get('username')?.value
+        };
+        this.userService.create(user).then(_ => {
+          console.log("User added successfully");
+        }).catch(error => {
+          console.error(error);
+        });
         this.router.navigateByUrl('/');
     }).catch(error => {
       console.error(error);
